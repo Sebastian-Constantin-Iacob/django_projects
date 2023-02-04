369144ff1292b51e2e3f63069f16a4daeef5a3ff
Constantin-Sebastian <Iacob1832@gmail.com>
2023-02-04 04:31:30 +0000

    Experiment with Django templating system

--- Study Bud/devsearch/projects/views.py
+++ Study Bud/devsearch/projects/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+
+
+
+def proejcts(request):
+    return render(request, 'projects.html')
+
+
+def project(request, pk):
+    return render(request, 'single-project.html')
+    
