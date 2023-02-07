56bbcfb13531ad19c52a4a7f1e77472d466eda3f
Constantin-Sebastian <Iacob1832@gmail.com>
2023-02-07 08:47:06 +0000

    CRUD operations

--- Study Bud/devsearch/projects/views.py
+++ Study Bud/devsearch/projects/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Project
 from .forms import ProjectForm
@@ -21,6 +21,32 @@ def project(request, pk):
 
 def createProject(request):
     form = ProjectForm()
+    # form process to the data base
+    if request.method == 'POST':
+        form = ProjectForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('projects')
     context = {'form': form}
     return render(request, 'projects/project_form.html', context)
     
+
+def updateProject(request, pk):
+    project = Project.objects.get(id=pk)
+    form = ProjectForm(instance=project)
+    # form process to the data base
+    if request.method == 'POST':
+        form = ProjectForm(request.POST, instance=project)
+        if form.is_valid():
+            form.save()
+            return redirect('projects')
+    context = {'form': form}
+    return render(request, 'projects/project_form.html', context)
+
+
+def deleteProject(request, pk):
+    project = Project.objects.get(id=pk)
+    if request.method == 'POST':
+        project.delete()
+        return redirect('projects')
+    return render(request, 'projects/delete_template.html', {'project': project})
