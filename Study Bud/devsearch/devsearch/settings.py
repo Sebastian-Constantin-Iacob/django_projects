c597781fd7eb51bd25433efb902ab2e085a41804
Constantin-Sebastian <Iacob1832@gmail.com>
2023-02-08 05:38:42 +0000

    White noise for static files
    A very interesting issue happened. I installed whitenoise from VScode, but Django could not find it. The fix was to install it from CMD. I do not understand why? May be worth exploring more ?

--- Study Bud/devsearch/devsearch/settings.py
+++ Study Bud/devsearch/devsearch/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-72674j&a8@fig+-5y0ypd7-%7=0ikt78!ikhd!&-*gz=p9yooj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -127,6 +128,7 @@ STATICFILES_DIRS = [
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
