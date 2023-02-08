f630f20873211221e1e296cec87e2d9fb457e180
Constantin-Sebastian <Iacob1832@gmail.com>
2023-02-08 10:45:01 +0000

    Add parts of chosen theme

--- Study Bud/devsearch/devsearch/settings.py
+++ Study Bud/devsearch/devsearch/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-72674j&a8@fig+-5y0ypd7-%7=0ikt78!ikhd!&-*gz=p9yooj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
