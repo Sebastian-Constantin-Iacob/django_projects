76ec070d6e8660289a6a479184c24efa43209045
Constantin-Sebastian <Iacob1832@gmail.com>
2023-02-09 16:32:33 +0000

    Finish setting up profile page front end

--- Study Bud/devsearch/users/views.py
+++ Study Bud/devsearch/users/views.py
@@ -8,7 +8,9 @@ def profiles(request):
     return render(request, 'users/profiles.html', context)
 
 
-def user_profile(request, pk):
+def userProfile(request, pk):
     profile = Profile.objects.get(id=pk)
-    context = {'profile': profile}
+    top_skills = profile.skill_set.exclude(description__exact='')
+    other_skills = profile.skill_set.filter(description = '')
+    context = {'profile': profile, 'top_skills': top_skills, 'other_skills': other_skills}
     return render(request, 'users/user-profile.html', context)
