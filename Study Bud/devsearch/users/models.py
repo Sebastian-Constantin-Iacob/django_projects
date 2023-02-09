73797794b446226e0489a6555b69b441f6a7e840
Constantin-Sebastian <Iacob1832@gmail.com>
2023-02-09 06:09:30 +0000

    Update model for project to have owner

--- Study Bud/devsearch/users/models.py
+++ Study Bud/devsearch/users/models.py
@@ -8,6 +8,7 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, blank=True, null=True)
     name = models.CharField(max_length=200, blank=True, null=True)
     email = models.EmailField(max_length=500, blank=True, null=True)
+    username = models.CharField(max_length=200, null=True, blank=True)
     short_intro = models.CharField(max_length=200, blank=True, null=True)
     bio = models.TextField(blank=True, null=True)
     profile_image = models.ImageField(null=True, blank=True, upload_to='profiles/', default='profiles/user-default.png')
