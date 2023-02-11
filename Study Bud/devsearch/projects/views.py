6efbe372e53d7bae444289af235fc9937a8da3ed
Constantin-Sebastian <Iacob1832@gmail.com>
2023-02-11 07:51:24 +0000

    Add messages for login/out

--- Study Bud/devsearch/projects/views.py
+++ Study Bud/devsearch/projects/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Project
 from .forms import ProjectForm
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
@@ -19,6 +20,7 @@ def project(request, pk):
     return render(request, 'projects/single-project.html', {'project': projectObj})
 
 
+@login_required(login_url="login")
 def createProject(request):
     form = ProjectForm()
     # form process to the data base
@@ -31,6 +33,7 @@ def createProject(request):
     return render(request, 'projects/project_form.html', context)
     
 
+@login_required(login_url="login")
 def updateProject(request, pk):
     project = Project.objects.get(id=pk)
     form = ProjectForm(instance=project)
@@ -44,6 +47,7 @@ def updateProject(request, pk):
     return render(request, 'projects/project_form.html', context)
 
 
+@login_required(login_url="login")
 def deleteProject(request, pk):
     project = Project.objects.get(id=pk)
     if request.method == 'POST':
--- Study Bud/devsearch/users/views.py
+++ Study Bud/devsearch/users/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.models import User
 from .models import Profile, Skill
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 # Create your views here.
 def profiles(request):
@@ -30,7 +32,7 @@ def loginUser(request):
         try:
             user = User.objects.get(username=username)
         except:
-            print("Username dose not exist")
+            messages.error(request, "Username dose not exist")
 
         user = authenticate(request, username=username, password=password)
 
@@ -38,11 +40,12 @@ def loginUser(request):
             login(request, user)
             return redirect("profiles")
         else:
-            print("User or Password dose not exist.")
+            messages.error(request, "User or Password dose not exist.")
 
     return render(request, 'users/login_register.html')
 
 
 def logoutUser(request):
     logout(request)
+    messages.error(request, "User was successfully logged out!")
     return redirect('login')
