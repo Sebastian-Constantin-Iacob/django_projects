30572d8c59ca487a5e4ae7c7d18dfa7b38391a47
Constantin-Sebastian <Iacob1832@gmail.com>
2023-03-02 05:27:50 +0000

    Add child search by skill

--- Study Bud/devsearch/users/views.py
+++ Study Bud/devsearch/users/views.py
@@ -13,8 +13,13 @@ def profiles(request):
 
     if request.GET.get('search_query'):
         search_query = request.GET.get('search_query')
-    print(f"Value: {search_query}")
-    profiles = Profile.objects.filter(Q(name__icontains = search_query) | Q(short_intro__icontains = search_query))
+        
+    skills = Skill.objects.filter(name__icontains=search_query)
+   
+    profiles = Profile.objects.distinct().filter(Q(name__icontains = search_query) | 
+                                      Q(short_intro__icontains = search_query) |
+                                      Q(skill__in=skills)
+                                      )
     context={'profiles': profiles, 'search_query': search_query}
     return render(request, 'users/profiles.html', context)
 
