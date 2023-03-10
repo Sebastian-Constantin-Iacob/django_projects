db4d5483611298f413256dc5c1e811b99cf66eaa
Constantin-Sebastian <Iacob1832@gmail.com>
2023-03-10 10:16:12 +0000

    Create database table for DMs

--- Study Bud/devsearch/users/models.py
+++ Study Bud/devsearch/users/models.py
@@ -42,4 +42,11 @@ class Skill(models.Model):
     
 
 class Message(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
     sender = models.ForeignKey(Profile, on_delete=models.SET_NULLL, null=True, blank=True)
+    recipient = models.ForeignKey(Profile, on_delete=models.SET_NULLL, null=True, blank=True, related_name="messages")
+    name = models.CharField(max_length=200, null=True, blank=True)
+    email = models.EmailField(max_length=200, null=True, blank=True)
+    subject = models.CharField(max_length=200, null=True, blank=True)
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
