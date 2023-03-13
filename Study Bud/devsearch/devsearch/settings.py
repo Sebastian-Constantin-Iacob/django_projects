88f7cc0c1a00d1f303fe65a9ac8ae38ca3e13992
Constantin-Sebastian <Iacob1832@gmail.com>
2023-03-13 10:33:11 +0000

    Set up email service ( settings.py )
    Obviously use evn variable not hard coded values.

--- Study Bud/devsearch/devsearch/settings.py
+++ Study Bud/devsearch/devsearch/settings.py
@@ -117,6 +117,13 @@ USE_I18N = True
 
 USE_TZ = True
 
+# Email configuration
+EMAIL_BACKEND = 'django.core.mail.backend.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'mytest1832@gmail.com'
+EMAIL_HOST_PASSWORD = ''
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
